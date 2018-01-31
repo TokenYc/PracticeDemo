c675d6c3dfce0a2c063b495bfb893f9ee5af36e4
yc <247067345@qq.com>
2018-01-31 10:20:07 +0800

    update

--- app/src/main/java/net/archeryc/demo/jumpTest/WebviewActivity.java
+++ app/src/main/java/net/archeryc/demo/jumpTest/WebviewActivity.java
@@ -1,19 +1,50 @@
 package net.archeryc.demo.jumpTest;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import net.archeryc.demo.R;
 
 public class WebviewActivity extends AppCompatActivity {
 
     WebView webview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        hideBottomUIMenu();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
         webview = findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
         webview.loadUrl("file:///android_asset/index.html");
+//        webview.loadUrl("http://bilibili.com");
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return true;
+            }
+        });
+    }
+
+    protected void hideBottomUIMenu() {
+        //隐藏虚拟按键，并且全屏
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
     }
 }
